5a47f365d33e72997270dfc9ccd6401478502aef
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-20 22:27:14 +0300

    Initial commit

---
+++ app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package theo.tziomakas.rxjavaudemytutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
