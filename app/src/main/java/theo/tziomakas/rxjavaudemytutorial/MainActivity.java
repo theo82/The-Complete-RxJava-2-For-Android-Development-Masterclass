cec96fbf2931edbd8776fc5230f12daf8261692b
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-21 12:00:30 +0300

    Composite Disposable

--- app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
+++ app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private String greeting = "Hello from RxJava";
     private Observable<String> myObservable;
     private DisposableObserver<String> myObserver;
+    private DisposableObserver<String> myObserver2;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     //private Disposable disposable;
 
     @Override
@@ -29,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         myObservable.subscribeOn(Schedulers.io());
 
         myObservable.observeOn(AndroidSchedulers.mainThread());
-
 
         myObserver = new DisposableObserver<String>() {
             @Override
@@ -48,7 +51,32 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        compositeDisposable.add(myObserver);
         myObservable.subscribe(myObserver);
+
+        myObserver2 = new DisposableObserver<String>(){
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "observer's 2 onNext invoked");
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "observer's 2 onError invoked");
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "observer's 2 onComplete invoked");
+
+            }
+        };
+
+        compositeDisposable.add(myObserver2);
+        myObservable.subscribe(myObserver2);
 
     }
 
@@ -56,6 +84,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         //disposable.dispose();
-        myObserver.dispose();
+        compositeDisposable.clear();
     }
 }
