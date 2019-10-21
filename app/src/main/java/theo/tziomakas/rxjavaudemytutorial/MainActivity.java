0514adfa35661ca1573684e0ada94168747fbcd6
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-21 10:25:04 +0300

    Lets start from zero

--- app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
+++ app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
@@ -3,12 +3,47 @@ package theo.tziomakas.rxjavaudemytutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private String greeting = "Hello from RxJava";
+    private Observable<String> myObservable;
+    private Observer<String> myObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myObservable = Observable.just(greeting);
+
+        myObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+               Log.d(TAG, "onSubsribe invoked");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext invoked");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError invoked");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete invoked");
+            }
+        };
+
+        myObservable.subscribe(myObserver);
     }
 }
