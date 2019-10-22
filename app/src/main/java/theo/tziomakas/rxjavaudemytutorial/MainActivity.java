ee8a622cbc2743d6b4f296d79e2ec92d7995ef0e
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-22 09:04:21 +0300

    create operator

--- app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
+++ app/src/main/java/theo/tziomakas/rxjavaudemytutorial/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -15,9 +19,8 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
-    private Integer[] greetings = {1,2,3,4,5};
-    private Observable<Integer> myObservable;
-    private DisposableObserver<Integer> myObserver;
+    private Observable<Student> myObservable;
+    private DisposableObserver<Student> myObserver;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
@@ -25,7 +28,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myObservable = Observable.range(0,20);
+
+        myObservable = Observable.create(new ObservableOnSubscribe<Student>() {
+            @Override
+            public void subscribe(ObservableEmitter<Student> emitter) throws Exception {
+                ArrayList<Student> studentArrayList = getStudents();
+
+                for(Student student:studentArrayList){
+                    emitter.onNext(student);
+                }
+
+                emitter.onComplete();
+            }
+        });
 
         compositeDisposable.add(
         myObservable
@@ -38,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
     DisposableObserver getObserver(){
 
-        myObserver = new DisposableObserver<Integer>() {
+        myObserver = new DisposableObserver<Student>() {
             @Override
-            public void onNext(Integer s) {
-                Log.d(TAG, "onNext invoked" + s);
+            public void onNext(Student s) {
+                Log.d(TAG, "onNext invoked" + s.getEmail());
             }
 
             @Override
@@ -56,6 +71,42 @@ public class MainActivity extends AppCompatActivity {
         };
 
         return myObserver;
+    }
+
+    private ArrayList<Student> getStudents(){
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student1 = new Student();
+        student1.setName(" student1 ");
+        student1.setEmail( " student1@gmail.com ");
+        student1.setAge(27);
+        students.add(student1);
+
+        Student student2 = new Student();
+        student2.setName(" student2 ");
+        student2.setEmail( " student2@gmail.com ");
+        student2.setAge(27);
+        students.add(student2);
+
+        Student student3 = new Student();
+        student3.setName(" student3 ");
+        student3.setEmail( " student3@gmail.com ");
+        student3.setAge(27);
+        students.add(student3);
+
+        Student student4 = new Student();
+        student4.setName(" student4 ");
+        student4.setEmail( " student4@gmail.com ");
+        student4.setAge(27);
+        students.add(student4);
+
+        Student student5 = new Student();
+        student5.setName(" student5 ");
+        student5.setEmail( " student5@gmail.com ");
+        student5.setAge(27);
+        students.add(student4);
+
+        return students;
     }
 
     @Override
